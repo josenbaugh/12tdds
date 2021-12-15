897427e7a2f21048bb683f4327bb37482ec33e32
Joshua Osenbaugh <josh@osenbaugh.xyz>
2021-12-15 11:12:47 -0500

    add test for making the game unplayable for less than 2 players

--- day6/tests/GameTest.php
+++ day6/tests/GameTest.php
@@ -10,4 +10,15 @@ require __DIR__ . '/../vendor/autoload.php';
 
 class GameTest extends TestCase
 {
+    /**
+     * @test
+    */
+    public function cannot_play_game_with_lessthan_2_players(): void
+    {
+        $game = new Game();
+        $game->add('Vader');
+
+        $this->expectException(\Exception::class);
+        $game->roll(rand(0,5) + 1);
+    }
 }
