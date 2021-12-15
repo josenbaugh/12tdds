6cbdfc11b500c488e834fbe02c156d302f971b96
Joshua Osenbaugh <josh@osenbaugh.xyz>
2021-12-15 15:44:57 -0500

    build out GameState to hold the logic for which state is current and what's next

--- day8/src/GameState.php
+++ day8/src/GameState.php
@@ -8,12 +8,21 @@ class GameState
     const ROLL = 'roll';
     const GUESS  = 'guess';
 
+    private $next_state = [
+        self::SETUP => self::ROLL,
+        self::ROLL => self::GUESS,
+        self::GUESS => self::ROLL,
+    ];
+
+    private $state = self::SETUP;
+
     public function getCurrentState(): string
     {
-        return '';
+        return $this->state;
     }
 
     public function next(): void
     {
+        $this->state = $this->next_state[$this->state];
     }
 }
