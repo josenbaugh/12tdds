e15a71d26c18a7cad0e4395a68e22dedf5368aae
Joshua Osenbaugh <josh@osenbaugh.xyz>
2021-12-15 12:22:26 -0500

    test that players can get out of jail, currently failing because of a bug

--- day7/tests/GameTest.php
+++ day7/tests/GameTest.php
@@ -13,12 +13,35 @@ class GameTest extends TestCase
     /**
      * @test
     */
-    public function cannot_play_game_with_lessthan_2_players(): void
+    public function cannot_play_game_with_less_than_2_players(): void
     {
         $game = new Game();
         $game->add('Vader');
 
         $this->expectException(\Exception::class);
         $game->roll(rand(0,5) + 1);
+    }
+
+    /**
+     * @test
+     */
+    public function players_can_get_out_of_jail(): void
+    {
+        $game = new Game();
+        $game->add('Vader');
+        $game->add('Maul');
+
+        $game->roll(2);
+        $game->wasCorrectlyAnswered(); //vader gets a coin
+
+        $game->roll(1);
+        $game->wrongAnswer(); //maul goes to the penalty box
+
+        $game->roll(2);
+        $game->wasCorrectlyAnswered(); //vader gets a coin
+
+        $game->roll(1); //odd roll gets maul out
+
+        self::assertFalse($game->inPenaltyBox[1]);
     }
 }
