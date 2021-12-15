4aa927d1761cb5d55754a0ce148c549d2028d483
Joshua Osenbaugh <josh@osenbaugh.xyz>
2021-12-15 13:21:28 -0500

    port day7

---
+++ day8/src/GameRunner.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace tdd;
+
+use tdd\Game;
+
+$notAWinner;
+
+  $aGame = new Game();
+  
+  $aGame->add("Chet");
+  $aGame->add("Pat");
+  $aGame->add("Sue");
+  
+  
+  do {
+    
+    $aGame->roll(rand(0,5) + 1);
+    
+    if (rand(0,9) == 7) {
+      $notAWinner = $aGame->wrongAnswer();
+    } else {
+      $notAWinner = $aGame->wasCorrectlyAnswered();
+    }
+    
+    
+    
+  } while ($notAWinner);
+  
---
+++ day8/tests/GameTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace tdd;
+
+use Generator;
+use PHPUnit\Framework\TestCase;
+use tdd\Game;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+class GameTest extends TestCase
+{
+    /**
+     * @test
+    */
+    public function cannot_play_game_with_less_than_2_players(): void
+    {
+        $game = new Game();
+        $game->add('Vader');
+
+        $this->expectException(\Exception::class);
+        $game->roll(rand(0,5) + 1);
+    }
+
+    /**
+     * @test
+     */
+    public function players_can_get_out_of_jail(): void
+    {
+        $game = new Game();
+        $game->add('Vader');
+        $game->add('Maul');
+
+        $game->roll(2);
+        $game->wasCorrectlyAnswered(); //vader gets a coin
+
+        $game->roll(1);
+        $game->wrongAnswer(); //maul goes to the penalty box
+        self::assertTrue($game->inPenaltyBox[1]);
+
+        $game->roll(2);
+        $game->wasCorrectlyAnswered(); //vader gets a coin
+
+        $game->roll(1); //odd roll gets maul out
+
+        self::assertFalse($game->inPenaltyBox[1]);
+    }
+}
